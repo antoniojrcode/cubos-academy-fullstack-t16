a36f3d5f1b917c17133648b083ecdd1d21deefdf
antoniojrcode <matos.antonioap@gmail.com>
2024-01-23 20:26:25 -0300

    novas anotações de comandos git

--- modulo01/gitBasico/aulaDeGit.js
+++ modulo01/gitBasico/aulaDeGit.js
@@ -5,7 +5,10 @@
 //git add - adiciona todos os arquivo ou um arquivo em específico ao stage area.
 //git reset - retira um arquivo ou todos do stage area para o unstage.
 //git commit - identifica ou classifica uma alteração enviada ao servidor remoto.
-//git push - envia os commit ao servidor remoto.
-//git pull - atualiza a versão do servidor remoto com o local
+//git push -u origin master(nome da branch) - envia os commit ao servidor remoto.
+//git pull origin master - atualiza a versão do servidor remoto com o local
+//git remote add origin - vincula o repositório local com um remoto.
+//git checkout -b nomeDaBranch - Vai criar uma nova ramificação
+//git checkout nomeDaBranch - vou navegar entre as ramificações.
 
-console.log("Aula de comandos de git")
+console.log("Aula de comandos de git");
