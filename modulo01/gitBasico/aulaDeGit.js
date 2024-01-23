39c976a1b3b9020480162fbe045e9f5417aaa6ad
antoniojrcode <matos.antonioap@gmail.com>
2024-01-23 19:46:22 -0300

    criacao de um novo arquivo do modulo 01

---
+++ modulo01/gitBasico/aulaDeGit.js
@@ -0,0 +1,11 @@
+//Comandos aprendidos na aula
+//git init - inicializa um repositório local
+//git clone - baixa/clona um repositório remoto(Ex: gitHub)
+//git status - verifica o status atual dos arquivos modificados
+//git add - adiciona todos os arquivo ou um arquivo em específico ao stage area.
+//git reset - retira um arquivo ou todos do stage area para o unstage.
+//git commit - identifica ou classifica uma alteração enviada ao servidor remoto.
+//git push - envia os commit ao servidor remoto.
+//git pull - atualiza a versão do servidor remoto com o local
+
+console.log("Aula de comandos de git")
