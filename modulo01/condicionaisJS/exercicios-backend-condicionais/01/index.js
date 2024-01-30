80f0767a6f74e572b6eeee4b09ca4d29648aaf55
antoniojrcode <matos.antonioap@gmail.com>
2024-01-30 19:51:19 -0300

    resolvido questão 01 de condicional

--- modulo01/condicionaisJS/exercicios-backend-condicionais/01/index.js
+++ modulo01/condicionaisJS/exercicios-backend-condicionais/01/index.js
@@ -1,4 +1,42 @@
-const jogada1 = "pedra"
+const jogada1 = "tesoura"
 const jogada2 = "tesoura"
 
 //seu código aqui
+
+if (jogada1 === jogada2) {
+  console.log(`Empate!`);
+} else if ((jogada1 == "pedra" && jogada2 == "tesoura") || (jogada2 == "pedra" && jogada1 == "tesoura")) {
+  console.log("Pedra ganha de Tesoura");
+} else if ((jogada1 == "papel" && jogada2 == "pedra") || (jogada2 == "papel" && jogada1 == "pedra")) {
+  console.log("Papel ganha de Pedra");
+} else if ((jogada1 == "tesoura" && jogada2 == "papel") || (jogada2 == "tesoura" && jogada1 == "papel")) {
+  console.log("Tesoura ganha de Papel");
+} else {
+  console.log("Valor digitado é inválido");
+}
+
+// versão não otimizada kkkk
+// if (jogada1 === jogada2) {
+//   console.log(`Empate!`);
+// } else if (jogada1 === "pedra" && jogada2 === "tesoura") {
+//   console.log(`${jogada1} ganha de ${jogada2}`);
+// } else if (jogada1 === "papel" && jogada2 === "pedra") {
+//   console.log(`${jogada1} ganha de ${jogada2}`);
+// } else if (jogada1 === "tesoura" && jogada2 === "papel") {
+//   console.log(`${jogada1} ganha de ${jogada2}`);
+// } else if (jogada2 === "pedra" && jogada1 === "tesoura") {
+//   console.log(`${jogada2} ganha de ${jogada1}`)
+// } else if (jogada2 === "papel" && jogada1 === "pedra") {
+//   console.log(`${jogada2} ganha de ${jogada1}`);
+// } else if (jogada2 === "tesoura" && jogada1 === "papel") {
+//   console.log(`${jogada2} ganha de ${jogada1}`);
+// } else {
+//   console.log(`valor inválido!`);
+// }
+
+
+
+
+//pedra ganha de tesoura
+//papel ganha de pedra
+//tesoura ganha de papel
