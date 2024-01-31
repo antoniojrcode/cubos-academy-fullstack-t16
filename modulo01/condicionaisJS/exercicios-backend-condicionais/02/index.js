318e8a34ff619e0096a34aab6990175c1e65d410
antoniojrcode <matos.antonioap@gmail.com>
2024-01-31 19:09:41 -0300

    resolução da questão 02 de par ou impar

--- modulo01/condicionaisJS/exercicios-backend-condicionais/02/index.js
+++ modulo01/condicionaisJS/exercicios-backend-condicionais/02/index.js
@@ -1,4 +1,11 @@
 const jogada1 = 5;
-const jogada2 = 3;
+const jogada2 = 7;
+const soma = jogada1 + jogada2; //soma dos dois valores das constantes.
 
 //seu código aqui
+
+if (soma % 2 === 0) { //Expressão para verificar se o valor é par.
+  console.log(`Jogador 1 ganhou! ${soma} é par!`);
+} else {
+  console.log(`Jogador 2 ganhou! ${soma} é ímpar!`);
+}
