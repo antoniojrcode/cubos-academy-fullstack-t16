d9c367a25a80873103cb0c74f81ae32d78fe5a56
antoniojrcode <matos.antonioap@gmail.com>
2024-01-31 19:30:09 -0300

    resolução da questão do jogo dominó

--- modulo01/condicionaisJS/exercicios-backend-condicionais/03/index.js
+++ modulo01/condicionaisJS/exercicios-backend-condicionais/03/index.js
@@ -1,4 +1,10 @@
 const ladoA = 3;
-const ladoB = 3;
+const ladoB = 2;
 
 //seu código aqui
+
+if (ladoA === ladoB) { //verificar se é uma bucha(carrão)
+  console.log("SIM");
+} else {
+  console.log("NÃO");
+}
