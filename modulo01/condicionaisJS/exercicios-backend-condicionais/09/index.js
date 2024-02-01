177accefa3645e10c9e84716a2f1e1bac2463825
antoniojrcode <matos.antonioap@gmail.com>
2024-02-01 00:13:04 -0300

    resolvido o exercicio de conversão da nota numérica para conceito de A a E

--- modulo01/condicionaisJS/exercicios-backend-condicionais/09/index.js
+++ modulo01/condicionaisJS/exercicios-backend-condicionais/09/index.js
@@ -1,1 +1,15 @@
-const nota = 8.5;
+const nota = 11;
+
+if (nota >= 0 && nota < 4) {
+  console.log(`Sua nota é: ${nota}. O estudante obteve o Conceito E`);
+} else if (nota >= 4 && nota < 6) {
+  console.log(`Sua nota é: ${nota}. O estudante obteve o Conceito D`);
+} else if (nota >= 6 && nota < 8) {
+  console.log(`Sua nota é: ${nota}. O estudante obteve o Conceito C`);
+} else if (nota >= 8 && nota < 9) {
+  console.log(`Sua nota é: ${nota}. O estudante obteve o Conceito B`);
+} else if (nota >= 9 && nota <= 10) {
+  console.log(`Sua nota é: ${nota}. O estudante obteve o Conceito A`);
+} else {
+  console.log("Valor informado é invalido! Só é permitido de 0 a 10.0 na nota!");
+}
