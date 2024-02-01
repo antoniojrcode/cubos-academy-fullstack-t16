cd27e1b2ea791787be04c3239c97097f37e01e5e
antoniojrcode <matos.antonioap@gmail.com>
2024-01-31 23:27:20 -0300

    resolução da questão de isenção de imposto de renda

--- modulo01/condicionaisJS/exercicios-backend-condicionais/07/index.js
+++ modulo01/condicionaisJS/exercicios-backend-condicionais/07/index.js
@@ -1,5 +1,15 @@
 const aposentada = false;
 const portadoraDeDoenca = false;
-const totalDeRendimentos = 3000000; //emCentavos
+const totalDeRendimentos = 2000000; //emCentavos
+const redimentoMinimo = 2855970;
 
 //seu código aqui
+
+//Nesse IF ele verifica se as duas variáveis são True
+if (aposentada || portadoraDeDoenca) {
+  console.log("ISENTA");
+} else if (totalDeRendimentos < redimentoMinimo) {
+  console.log("VAZA LEAO! JA TA DIFICIL SEM VOCE");
+} else {
+  console.log("PEGA LEAO");
+}
