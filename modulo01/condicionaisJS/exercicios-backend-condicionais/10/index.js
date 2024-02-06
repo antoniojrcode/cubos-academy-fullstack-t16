3a8ec6452653ead06434308059bc4e5241e9f752
antoniojrcode <matos.antonioap@gmail.com>
2024-02-06 01:02:08 -0300

    correcao da questao de verificar vogal e consoante e numeral

--- modulo01/condicionaisJS/exercicios-backend-condicionais/10/index.js
+++ modulo01/condicionaisJS/exercicios-backend-condicionais/10/index.js
@@ -1,1 +1,11 @@
-const caractere = "E";
+const caractere = "a";
+
+if (caractere === "A" || caractere === "E" || caractere === "I" || caractere === "O" || caractere === "U") {
+  console.log("Vogal maiúscula");  
+} else if (caractere === "a" || caractere === "e" || caractere === "i" || caractere === "o" || caractere === "u") {
+  console.log("Vogal minúscula");
+} else if (isNaN(caractere) === false) { //isNaN() vai verificar que se for um número, ele vai retornar "false"
+  console.log("isso é um Número");
+} else {
+  console.log("A letra é uma consoante!");
+}
