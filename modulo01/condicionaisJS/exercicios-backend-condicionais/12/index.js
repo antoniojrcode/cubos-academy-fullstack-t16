1fe6e0ad7a9d9607087854e40df9b1a6e56add3e
antoniojrcode <matos.antonioap@gmail.com>
2024-02-07 19:06:28 -0300

    resolucao da questao do dia da semana usando if e switch

--- modulo01/condicionaisJS/exercicios-backend-condicionais/12/index.js
+++ modulo01/condicionaisJS/exercicios-backend-condicionais/12/index.js
@@ -1,1 +1,29 @@
-const diaDaSemana = 3;
+const diaDaSemana = 9;
+
+if (diaDaSemana >= 1 && diaDaSemana <= 7) {
+  switch(diaDaSemana) {
+    case 1 :
+      console.log("Hoje é segunda-feira");
+      break;
+    case 2 :
+      console.log("Hoje é terça-feira");
+      break;
+    case 3 :
+      console.log("Hoje é quarta-feira");
+      break;
+    case 4 :
+      console.log("Hoje é quinta-feira");
+      break;
+    case 5 :
+      console.log("Hoje é sexta-feira");
+      break;
+    case 6 :
+      console.log("Hoje é sábado");
+      break;
+    case 7 :
+      console.log("Hoje é domingo");
+      break;
+  }
+} else {
+  console.log(`O valor ${diaDaSemana} não é um dia da semana!`);
+}
