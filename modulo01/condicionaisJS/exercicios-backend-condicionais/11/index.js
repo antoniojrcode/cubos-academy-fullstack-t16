60f1041a2e5f513c504b034c3f8c3d85d13e6b04
antoniojrcode <matos.antonioap@gmail.com>
2024-02-07 18:53:37 -0300

    resolucao da questao do valor do curso do aluno

--- modulo01/condicionaisJS/exercicios-backend-condicionais/11/index.js
+++ modulo01/condicionaisJS/exercicios-backend-condicionais/11/index.js
@@ -1,8 +1,30 @@
 //renda mensal do aluno, em centavos.
-const rendaMensalEmCentavos = 300000;
+const rendaMensalEmCentavos = 2000;
+
+// calcula o valor da parcela do curso em 18%
+const valorParcelaCurso = rendaMensalEmCentavos * 0.18;
 
 // Tempo decorrido de contrato. Se for maior que 60 meses, o aluno não deve mais nada.
-const mesesDecorridos = 12;
+const mesesDecorridos = 51;
 
-// Soma das parcelas já pagas pelo aluno nos meses anteriores (em centavos). Se for igual a 18 mil reais, o aluno não deve pagar mais nada, pois já quitou a dívida.
-const totalJaPagoPeloAluno = 1000000;
+// Soma das parcelas já pagas pelo aluno nos meses anteriores 
+//(em centavos). Se for igual a 18 mil reais, o aluno não deve 
+//pagar mais nada, pois já quitou a dívida.
+const totalJaPagoPeloAluno = mesesDecorridos * valorParcelaCurso;
+
+if (mesesDecorridos <= 60) {
+  if (rendaMensalEmCentavos >= 2000) {
+    if (totalJaPagoPeloAluno < 18000) {
+      console.log(`O seu salário atual é R$${rendaMensalEmCentavos}`);
+      console.log(`Valor da parcela R$${valorParcelaCurso}`); 
+    } else {
+      console.log(`Você já pagou R$${totalJaPagoPeloAluno} dos R$18.000 do seu curso e ele está quitado!`);
+    }
+  } else {
+    console.log(`Seu salário atual é inferior a R$2000`);
+  }
+} else {
+  console.log(`Sua dívida está perdoada, pois já foram pagos ${mesesDecorridos} meses dos 60 meses de contrato`);
+}
+
+
