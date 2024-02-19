2393f0170abf801c8f13432618b1ba5dae94db9d
antoniojrcode <matos.antonioap@gmail.com>
2024-02-19 19:17:22 -0300

    resolucao da questao 13

--- modulo01/condicionaisJS/exercicios-backend-condicionais/13/index.js
+++ modulo01/condicionaisJS/exercicios-backend-condicionais/13/index.js
@@ -1,6 +1,26 @@
 //tipo de pagamento (dinheiro, credito, debito, cheque).
-const tipoDePagamento = "credito";
+const tipoDePagamento = "debit";
 
 //valor da mercadoria (centavos)
 const valorDoProduto = 13000;
 
+let valorFinal;
+
+if (tipoDePagamento.toUpperCase() === "CREDITO") {
+  valorFinal = valorDoProduto - (valorDoProduto * 0.05);
+  console.log(`Você pagou no crédito!`);
+  console.log(`Valor do produto R$${valorDoProduto.toFixed(2)}`);
+  console.log(`Valor com 5% de DESCONTO R$${valorFinal.toFixed(2)}`);
+} else if (tipoDePagamento.toUpperCase() === "CHEQUE") {
+  valorFinal = valorDoProduto - (valorDoProduto * 0.03);
+  console.log(`Você pagou no cheque!`);
+  console.log(`Valor do produto R$${valorDoProduto.toFixed(2)}`);
+  console.log(`Valor com 3% de DESCONTO R$${valorFinal.toFixed(2)}`);
+} else if (tipoDePagamento.toUpperCase() === "DEBITO" || tipoDePagamento.toUpperCase() === "DÉBITO" || tipoDePagamento.toUpperCase() === "DINHEIRO") {
+  valorFinal = valorDoProduto - (valorDoProduto * 0.10);
+  console.log(`Você escolheu pagar à vista.`);
+  console.log(`Valor do produto R$${valorDoProduto.toFixed(2)}`);
+  console.log(`Valor com 10% de DESCONTO R$${valorFinal.toFixed(2)}`);
+} else {
+  console.log(`Sua forma de pagamento ainda não é aceita no momento!`);
+}
